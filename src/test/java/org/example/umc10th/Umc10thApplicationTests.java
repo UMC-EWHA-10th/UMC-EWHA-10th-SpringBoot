01688fe51d0046cfa29d7572370a30592349b419
m00nsh <cowhmoon@gmail.com>
2026-04-08 13:25:32 +0900

    week4 작업 생성

---
+++ src/test/java/org/example/umc10th/Umc10thApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.umc10th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc10thApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
