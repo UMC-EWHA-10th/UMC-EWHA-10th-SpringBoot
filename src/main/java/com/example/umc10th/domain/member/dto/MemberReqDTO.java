2d11b8b537b340323ff1fdd9c29a321053203158
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-05-01 03:38:11 +0900

    week5 과제

--- src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
+++ src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
@@ -2,11 +2,6 @@ package com.example.umc10th.domain.member.dto;
 
 public class MemberReqDTO {
 
-    //마이페이지
-//    public record GetInfo(
-//            Long id
-//    ){}
-
     public record GetInfo(
             Long id
     ){}
--- src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
+++ src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
@@ -1,4 +1,18 @@
 package com.example.umc10th.domain.mission.dto;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
 public class MissionReqDTO {
+
+    // 미션 성공 처리
+    public record CompleteMission(
+            @NotBlank
+            String status   // "COMPLETED"
+    ) {}
 }
