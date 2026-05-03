ac24db8be867c9fc751eda2fb17f94a7f768da07
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-05-03 23:58:45 +0900

    week6 과제

--- src/main/java/com/example/umc10th/domain/mission/entity/mapping/MemberMission.java
+++ src/main/java/com/example/umc10th/domain/mission/entity/mapping/MemberMission.java
@@ -1,4 +1,32 @@
 package com.example.umc10th.domain.mission.entity.mapping;
 
+import com.example.umc10th.domain.member.entity.Member;
+import com.example.umc10th.domain.mission.entity.Mission;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member_mission")
 public class MemberMission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ColumnDefault("0")
+    @Column(name = "is_complete", nullable = false)
+    private Boolean isComplete;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id", nullable = false)
+    private Mission mission;
 }
--- src/main/java/com/example/umc10th/domain/review/entity/Reply.java
+++ src/main/java/com/example/umc10th/domain/review/entity/Reply.java
@@ -1,4 +1,20 @@
 package com.example.umc10th.domain.review.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "reply")
 public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
 }
