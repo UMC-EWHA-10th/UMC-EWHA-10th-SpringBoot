79bc70eac2150e86e4f588b5538d657a34c95ade
SeoHyeon <shyeon16@ewha.ac.kr>
2026-05-03 22:18:50 +0900

    [Feat] Mission Enum 정의

--- src/main/java/com/example/umc10th/domain/mission/enums/Address.java
+++ src/main/java/com/example/umc10th/domain/mission/enums/Address.java
@@ -1,4 +1,16 @@
 package com.example.umc10th.domain.mission.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Address {
+    ANAM("안암동"),
+    SAMSUNG("삼성동"),
+    YEOUIDO("여의도동"),
+    PANGYO("판교동"),
+    SHINCHON("신촌동");
+
+    private final String description;
 }
